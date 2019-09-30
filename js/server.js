1bcd778113c66f7c3554f413af0db23614222b69
jbsneed <52945300+jbsneed@users.noreply.github.com>
2019-09-30 00:33:24 -0500

    fixed error

--- js/server.js
+++ js/server.js
@@ -6,12 +6,12 @@ http
   .createServer((request, response) => {
     var addr = request.url,
       q = url.parse(addr, true),
-      filepath = "";
+      filePath = "";
 
     if (q.pathname.includes("documentation")) {
       filePath = __dirname + "/documentation.html";
     } else {
-      filepath = "index.html";
+      filePath = "index.html";
     }
     fs.readFile(filePath, function(err, data) {
       if (err) {
@@ -19,19 +19,14 @@ http
       }
       response.writeHead(200, { "Content-Type": "text/html" });
       response.write(data);
+      fs.appendFile("log.txt", "URL: " + addr + "/n" + "Timestamp: " + new Date() + "/n/n", function(err){
+        if (err){
+          console.log(err)
+        } else{
+          console.log("Added to log.");
+        }
+      })
       response.end();
-    });
-  })
+          });
+    })
   .listen(8080);
-
-fs.appendFile(
-  "log.txt",
-  "URL: " + addr + "\nTimestamp: " + new Date() + "\n\n",
-  function(err) {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log("Added to log.");
-    }
-  }
-);
