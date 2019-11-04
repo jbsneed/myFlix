d68276688cb011998067ddeb4a6f67f23e631d5d
jbsneed <52945300+jbsneed@users.noreply.github.com>
2019-11-03 23:08:10 -0600

    added return mainView

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { MainView } from './components/main-view/main-view';
+
 //Import statement to indicate that you need to bundle './index.scss'
 import './index.scss';
 
 //Main component (will eventually use all the others)
-class MyFlixApplication extends React.Component{
-  render(){
-    return(
-      <div className="my-flix">
-        <div>Good morning</div>
-        </div>
-    );
+class MyFlixApplication extends React.Component {
+  render() {
+    return <MainView />;
   }
 }
 
@@ -19,4 +17,4 @@ class MyFlixApplication extends React.Component{
 const container = document.getElementsByClassName('app-container')[0];
 
 //Tells React to render your app in the root DOM element
-ReactDOM.render(React.createElement(MyFlixApplication),container);
+ReactDOM.render(React.createElement(MyFlixApplication), container);
