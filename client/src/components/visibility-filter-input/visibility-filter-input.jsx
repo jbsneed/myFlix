cc1c21ad5d18e935dd1e31cd0014b247a51a8d5d
jbsneed <52945300+jbsneed@users.noreply.github.com>
2019-11-27 01:45:58 -0600

    changed position of search bar

--- client/src/components/visibility-filter-input/visibility-filter-input.jsx
+++ client/src/components/visibility-filter-input/visibility-filter-input.jsx
@@ -2,15 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
+import './visibility-filter-input.scss'
+
+
 import Form from 'react-bootstrap/Form';
 
 import { setFilter } from '../../actions/actions';
 
 function VisibilityFilterInput(props) {
-  return <Form.Control
+  return <Form.Control className="search-bar"
     onChange={e => props.setFilter(e.target.value)}
     value={props.visibilityFilter}
-    placeholder="filter"
+    placeholder="Search"
   />;
 }
 
